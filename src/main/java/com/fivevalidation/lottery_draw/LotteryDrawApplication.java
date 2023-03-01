d6ef0ee3ad60078b56aec6c18204b2124cba87f0
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-03-01 10:35:55 -0300

    initial commit, project structure

---
+++ src/main/java/com/fivevalidation/lottery_draw/LotteryDrawApplication.java
@@ -0,0 +1,13 @@
+package com.fivevalidation.lottery_draw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryDrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryDrawApplication.class, args);
+	}
+
+}
